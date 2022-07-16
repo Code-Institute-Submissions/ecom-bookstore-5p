1d558973015d721f62e8e9f038302f29b189ec63
Enrique De Nobrega <edenobrega@sky.com>
2022-07-16 20:59:47 +0000

    Make empty CRUD for books models, and link urls

--- bookstoreadmin/urls.py
+++ bookstoreadmin/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 import bookstoreadmin.views as bsav
 
 urlpatterns = [
-    path('', bsav.Index.as_view(), name='admin_index')
+    path('', bsav.index.as_view(), name='admin_index'),
+    path('books/', bsav.list_books.as_view(), name='list_books_admin'),
+    path('books/modify/<int:book_id>', bsav.modify_book.as_view(), name='modify_book'),
+    path('books/create/', bsav.create_book.as_view(), name='create_book'),
+    path('books/delete/<int:book_id>', bsav.delete_book, name='delete_book'),
+    path('genres/', bsav.list_genres.as_view(), name='list_genres_admin'),
 ]
--- bookstoreadmin/views.py
+++ bookstoreadmin/views.py
@@ -1,7 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import permission_required
+import books.models as bkm
+
+PREFIX = 'bookstoreadmin/'
+#region helpers
+def check_admin(logged_user):
+    return logged_user.is_superuser or Group.objects.filter(
+            user=logged_user,
+            name='site_admin'
+        ).exists()
 
 
-class Index(View):
+class index(View):
     def get(self, request):
-        return render(request, 'bookstoreadmin/index.html')
+        if check_admin(request.user):
+            return render(request, PREFIX+'index.html')
+        return redirect('books_index')
+#endregion
+
+
+#region books
+class list_books(PermissionRequiredMixin, View):
+    permission_required = 'books.view_book'
+
+    def get(self, request):
+        obj = bkm.Book.objects.all()
+        return render(
+            request,
+            PREFIX+'books/books.html',
+            {
+                'obj': obj
+            }
+        )
+
+    def post(self, request):
+        pass
+
+
+class create_book(PermissionRequiredMixin, View):
+    permission_required = 'books.create_book'
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class modify_book(PermissionRequiredMixin, View):
+    permission_required = 'books.change_book'
+
+    def get(self, request, book_id):
+        return render(request, PREFIX+'modify.html')
+
+    def post(self, request, book_id):
+        return redirect('admin_index')
+
+@permission_required('books.delete_book')
+def delete_book(request, book_id):
+    pass
+#endregion
+
+
+class list_genres(PermissionRequiredMixin, View):
+    pass
