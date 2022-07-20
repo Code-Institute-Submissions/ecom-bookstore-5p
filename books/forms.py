b701a60f89f207b012ee41368598b96e90d6f109
Enrique De Nobrega <edenobrega@sky.com>
2022-07-20 21:48:57 +0000

    Implement book search views

--- books/forms.py
+++ books/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 import books.models as bkm
 
 class SearchForm(forms.Form):
     name = forms.CharField(max_length=200, required=False)
     genre = forms.ModelChoiceField(
-        queryset=bkm.Genre.objects.all().order_by('id')
+        queryset=bkm.Genre.objects.all().order_by('id'),
+        required=False
     )
     author = forms.CharField(max_length=100, required=False)
+
+    # https://docs.djangoproject.com/en/4.0/ref/models/instances/#django.db.models.Model.clean
+    def clean(self):
+        a = not bool(self.cleaned_data['name'])
+        b = not bool(self.cleaned_data['author'])
+        c = not bool(self.cleaned_data['genre'])
+
+        if a and b and c:
+            raise ValidationError('Atleast One Input Must Be Filled')
--- books/views.py
+++ books/views.py
@@ -1,7 +1,62 @@
+from difflib import SequenceMatcher
 from django.shortcuts import render
 from django.views import View
+import books.forms as forms
+import books.models as bkm
+
+# https://stackoverflow.com/a/17388505
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 
 class Index(View):
     def get(self, request):
-        return render(request, 'books/index.html')
+        form = forms.SearchForm()
+        return render(
+            request,
+            'books/index.html',
+            {
+                'form': form
+            }
+        )
+
+    def post(self, request):
+        form = forms.SearchForm(request.POST)
+
+        if form.is_valid():
+            books = bkm.Book.objects.all()
+
+            valid = False
+            matches = []
+            for b in books:
+                # https://stackoverflow.com/a/19794198
+                if form.cleaned_data['name']:
+                    rating = similar(form.cleaned_data['name'], b.name)
+                    valid = rating > 0.75
+
+                book_genres = bkm.BookGenre.objects.filter(book=b)
+                book_genres = [x.genre.id for x in list(book_genres)]
+                if form.cleaned_data.get('genre') is not None:
+                    valid = form.cleaned_data.get('genre').id in book_genres
+
+                if form.cleaned_data['author']:
+                    rating = similar(form.cleaned_data['author'], b.author)
+                    valid = rating > 0.75
+
+                if valid:
+                    matches.append(b)
+            
+            return render(
+                request,
+                'books/results.html',
+                {
+                    'data': matches
+                }
+            )
+        return render(
+            request,
+            'books/index.html',
+            {
+                'form': form
+            }
+        )
