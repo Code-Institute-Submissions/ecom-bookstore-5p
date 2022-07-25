f60221394fca24620b972a3b00ca2026ab35e0ec
Enrique De Nobrega <edenobrega@sky.com>
2022-07-25 03:24:07 +0000

    Display order on checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
-import books.models as bkm 
+from django.conf import settings
+import checkout.forms as forms
+import books.models as bkm
+import stripe
 import os
 if os.path.exists('env.py'):
     import env  # noqa
@@ -9,27 +12,52 @@ if os.path.exists('env.py'):
 class checkout(View):
     def get(self, request):
         if request.session.get('basket', False):
+            # os.environ.get('STRIPE_PUBLIC_KEY')
+            # os.environ.get('STRIPE_SECRET_KEY')
+
             basket = request.session['basket']
             total = 0
-            prices = {}
 
+            template_basket = []
+
+            # Something wierd with price calc, idk why
             for bookid, quantity in basket.items():
-                book =  bkm.Book.objects.get(id=bookid)
-                price = (book.price * (1 - book.discountPercent)) * quantity
+                book = bkm.Book.objects.get(id=bookid)
+                price = (float(book.price) * (1 - book.discountPercent/100)) * quantity
                 total += price
-                prices['bookid'] = price
-            
-            total = round(total * 100)
+
+                template_basket.append(
+                    [
+                        book.name,
+                        book.price,
+                        str(book.discountPercent) + '%',
+                        quantity,
+                        format(price, '.2f')
+                    ]
+                )
+           
+            stripe_total = round(total * 100)
+            stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY
+            )
+
+            print(intent)
         else:
             basket = {}
+
+        form = forms.OrderForm()
+
         return render(
             request,
             'checkout/checkout.html',
             {
                 'stripe_public_key': os.environ.get('STRIPE_PUBLIC_KEY'),
-                'client_secret': 'hehe_secret key',
-                'basket': basket,
-                'prices': prices
+                'client_secret': intent.client_secret,
+                'template_basket': template_basket,
+                'stripe_total': stripe_total,
+                'form': form
             }
         )
 
