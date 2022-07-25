55f494496cda5e1ae42f2791613cc6b1be74d972
Enrique De Nobrega <edenobrega@sky.com>
2022-07-25 06:09:22 +0000

    Add urls for new checkout views

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,4 +3,5 @@ import checkout.views as cv
 
 urlpatterns = [
     path('', cv.checkout.as_view(), name='checkout_page'),
+    path('payment/<int:order_id>', cv.checkout_payment.as_view(), name='checkout_payment'),
 ]
