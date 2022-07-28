c298d743e721c676dea69a21e38fd64c23d531f4
Enrique De Nobrega <edenobrega@sky.com>
2022-07-28 03:52:46 +0000

    Rename templatetag to better fit its function

--- basket/templatetags/tempmath.py
+++ basket/templatetags/tempmath.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter(name='div')
-def div(value1, value2):
-    return value1/int(value2)
+def convert_total(value1, value2):
+    return format(value1/int(value2),".2f")
