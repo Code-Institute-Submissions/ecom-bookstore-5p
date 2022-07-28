025364aaad0ae9986a81c12edb9f8a4617efa284
Enrique De Nobrega <edenobrega@sky.com>
2022-07-28 06:53:02 +0000

    Make checkout form fields required

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,10 +3,10 @@ from django_countries.fields import CountryField
 
 class OrderForm(forms.Form):
     order_id = forms.IntegerField(widget=forms.HiddenInput())
-    first_name = forms.CharField(max_length=30, required=False)
-    second_name = forms.CharField(max_length=30, required=False)
-    email = forms.EmailField(required=False)
-    country = forms.CharField(required=False)
-    postcode = forms.CharField(max_length=10, required=False)
-    address_line_one = forms.CharField(max_length=200, required=False)
-    town_city = forms.CharField(max_length=200, required=False)
+    first_name = forms.CharField(max_length=30, required=True)
+    second_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+    country = forms.CharField(required=True)
+    postcode = forms.CharField(max_length=10, required=True)
+    address_line_one = forms.CharField(max_length=200, required=True)
+    town_city = forms.CharField(max_length=200, required=True)
