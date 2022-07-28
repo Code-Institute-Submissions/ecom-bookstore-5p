1af53bd502abaa197cdce1db8b4d7990714a0143
Enrique De Nobrega <edenobrega@sky.com>
2022-07-28 09:55:07 +0000

    Allow users to remove items from basket

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
-import books.models as bkm
 from decimal import Decimal
+import books.models as bkm
 # Basket data stored as:
 #   {'book_id': quantity(int)}
 
@@ -13,11 +13,16 @@ class index(View):
         if request.session.get('basket', False):
             basket = request.session['basket']
             total = request.session['total']
+
+        template_data = []
+        for key, value in basket.items():
+            data = bkm.Book.objects.get(id=key)
+            template_data.append((data, value))
         return render(
             request,
             'basket/index.html',
             {
-                'basket': basket,
+                'basket': template_data,
                 'total': total
             }
         )
@@ -53,6 +58,9 @@ class modify(View):
             del request.session['basket'][str(id)]
 
         request.session.modified = True
+        if redirect_url == 'view_book':
+            # TODO: message saying you have added to basket not working
+            return redirect(redirect_url, id)
         return redirect(redirect_url)
 
 
@@ -60,7 +68,8 @@ class remove(View):
     def get(self, request, id):
         if str(id) in request.session['basket']:
             book = bkm.Book.objects.get(id=id)
-            request.session['total'] -= round((book.price * (1 - book.discountPercent)) * request.session['basket'][str(id)], 2)*100
+            price_of_single = book.price * (1 - Decimal(book.discountPercent/100))
+            request.session['total'] -= int(round(price_of_single * request.session['basket'][str(id)], 2)*100)
             del request.session['basket'][str(id)]
             request.session.modified = True
         return redirect('basket_index')
@@ -72,5 +81,5 @@ class clear(View):
             del request.session['basket']
         if request.session.get('total', False):
             del request.session['total']
-            request.session.modified = True
+        request.session.modified = True
         return redirect('basket_index')
