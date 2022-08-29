285a54cbbdab1a47d95e86a6cd5120f4ae9cd9a8
Enrique De Nobrega <edenobrega@sky.com>
2022-08-29 22:09:01 +0000

    Add default text to newsletter for canceling signup

--- postoffice/views.py
+++ postoffice/views.py
@@ -45,6 +45,7 @@ class write_newsletter(View):
                             subject=form.cleaned_data['subject'],
                             body=(
                                 form.cleaned_data['body'] +
+                                '\n Click below to stop receiving newsletters. \n' +
                                 f'\n{current_site.domain}/'
                                 f'cancel_newsletter/{e.id}'
                             ),
